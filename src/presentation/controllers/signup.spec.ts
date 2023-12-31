e92ebf4390d4fd64525a2ef40a5c101597e8d7aa
Daniel Fiuza <danielfiuza01@gmail.com>
2023-12-31 11:38:44 -0300

    refactor: set protocols and errors imports in one file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,6 @@
-import { InvalidParamError } from "../errors/invalid-param-error";
-import { MissingParamError } from "../errors/missing-param-error";
+import { MissingParamError, InvalidParamError } from "../errors";
 import { ServerError } from "../errors/server-error";
-import { EmailValidator } from "../protocols/email-validator";
+import { EmailValidator } from "../protocols";
 import { SignUpController } from "./signup";
 
 interface StutTypes {
