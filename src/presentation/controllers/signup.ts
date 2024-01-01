436cbea7b61d7c18cb97e1bffce8c0d50f214d94
Daniel Fiuza <danielfiuza01@gmail.com>
2024-01-01 12:40:03 -0300

    feat: add password confirmation validation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -12,12 +12,21 @@ export class SignUpController implements Controller {
 
   handle(httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ["name", "email", "password"];
+      const requiredFields = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
 
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
+      }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError("passwordConfirmation"));
       }
 
       if (!this.emailValidator.isValid(httpRequest.body.email)) {
